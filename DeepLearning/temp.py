cce3d966b0038bcd1913555368add620dd8e97dc
Seohyun-Roh <radult951@gmail.com>
2021-09-18 00:09:33 +0900

    딥러닝 실습 내용 정리

--- DeepLearning/temp.py
+++ DeepLearning/temp.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-x=np.array([[1,2],[3,4]])
+'''x=np.array([[1,2],[3,4]])
 y=np.array([[5,6],[7,8]])
 
 print(np.concatenate((x,y),axis=1))
@@ -60,4 +60,93 @@ numpy 곱셈 ( * )-> 행렬의 곱셈과는 다르다.;;
 
 scores=np.array([[99,93,60],[98,82,93],[93,65,81],[78,82,81]])
 print(scores.mean(axis=0)) #0 -> 가로 평균
-print(scores.mean(axis=1)) #1 -> 세로 평균
+print(scores.mean(axis=1)) #1 -> 세로 평균 '''
+
+print(np.random.seed(100))
+print(np.random.rand(5))
+print(np.random.rand(5,3)) #5행 3열
+
+###
+
+print(np.random.randn(5)) #음수, 양수 섞어서 나옴
+
+###
+
+m, sigma = 10,2
+print(m+sigma*np.random.randn(5))
+
+###
+
+mu, sigma = 0, 0.1
+print(np.random.normal(mu, sigma, 5)) #평균, 표준편차
+
+###
+
+a=np.array([11,11,12,13,14,15,16,17,12,13,11,14,18,19,20])
+unique_values = np.unique(a) #중복되는 값 제거
+print(unique_values)
+
+###
+
+#2차원을 1차원으로 바꿀 때
+x=np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
+print(x.flatten())
+
+###
+
+import pandas as pd
+
+x=pd.read_csv('countries.csv', header=0).values
+print(x)
+
+df=pd.DataFrame(x)
+print(df)
+
+df.to_csv('test.csv')'''
+
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+#X=["Mon", "Tue", "Wed", "Thur","Fri", "Sat","Sun"]
+
+'''Y1=[15.6, 14.2, 16.3, 18.2, 17.1, 20.2, 22.4]
+Y2=[20.1, 23.1, 23.8, 25.9, 23.4, 25.1, 26.3]
+
+plt.plot(X, Y1, label="Seoul")
+plt.plot(X, Y2, label="Busan")
+plt.xlabel("day")
+plt.ylabel("temperature")
+plt.legend(loc="lower right") #범례를 어디다 놓을지
+plt.title("Temperatures of Cities") #표이름
+plt.show() #그래프가 그려짐
+
+###
+
+plt.plot(X, [15.6,14.2,16.3, 18.2, 17.1, 20.2, 22.4], "sb") #s->square mark 네모 표시자 m-> magenta, g->green
+plt.show()
+
+###
+
+numbers=np.random.normal(size=10000)
+
+plt.hist(numbers)
+plt.xlabel("value")
+plt.ylabel("freq")
+plt.show()
+
+###
+
+def sigmoid(x):
+    s=1/(1+np.exp(-x))
+    ds=s*(1-s)
+    return s, ds
+
+X= np.linspace(-10, 10, 100)
+Y1, Y2 = sigmoid(X)
+
+plt.plot(X, Y1, label="Sigmoid")
+plt.plot(X, Y2, label="Sigmoid'")
+plt.xlabel("x")
+plt.ylabel("Sigmoid(X), Sigmoid'(X)")
+plt.legend(loc="upper left")
+plt.show()
