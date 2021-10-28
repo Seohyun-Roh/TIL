012ef88e6e29814189205e26320244bc74f93f56
Seohyun Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2021-10-28 17:42:17 +0900

    미니배치 & opencv 실습

--- DeepLearning/temp.py
+++ DeepLearning/temp.py
@@ -1,75 +1,129 @@
 '''
-# 6장 연습문제 2번
-import matplotlib.pyplot as plt
+# p.246 미니배치 실습
+
 import numpy as np
+import tensorflow as tf
 
-x = 10  
-learning_rate = 0.01
-precision = 0.00001  
-max_iterations = 2000
+# 데이터를 학습 데이터와 테스트 데이터로 나눈다. 
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-# 손실함수를 람다식으로 정의한다. 
-loss_func = lambda x: x**2-6*x+4
-# 그래디언트를 람다식으로 정의한다. 손실함수의 1차 미분값이다. 
-gradient = lambda x: 2*x-6
+data_size = x_train.shape[0]
+batch_size = 12	# 배치 크기
 
-X = np.arange(0.0, 10.0, 0.01)
-plt.plot(X, loss_func(X),'b-')
-
-x0 = 10
-plt.scatter(x0, loss_func(x0), c='orange')
-
-# 그래디언트 강하법
-for i in range(max_iterations):
-    x = x - learning_rate * gradient(x)
-    plt.scatter(x, loss_func(x), c='orange')
-    print("손실함수값(", x, ")=", loss_func(x))
-
-plt.show()
-print("최소값 = ", x)'''
+selected = np.random.choice(data_size, batch_size)
+print("p.246 미니배치 실습")
+print(selected)
+x_batch = x_train[selected]
+y_batch = y_train[selected]
 
 ###
 
-# 6장 연습문제 6번
+# p.250 미니배치 구현
 import numpy as np
 
-# 시그모이드 함수
-#def actf(x):
-#	return 1/(1+np.exp(-x))
-
-
-# ReLU 함수
 def actf(x):
-    return np.maximum(0, x)
+    return 1/(1+np.exp(-x))
 
-# tanH 함수
-#def actf(x):
-#    return (np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x))
+def actf_deriv(x):
+    return x*(1-x)
 
-# 입력유닛의 개수, 은닉유닛의 개수, 출력유닛의 개수
 inputs, hiddens, outputs = 2, 2, 1
-learning_rate=0.2
+learning_rate = 0.5
 
-# 훈련 샘플과 정답 
-X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+X = np.array([[0,0], [0,1], [1,0], [1,1]])
 T = np.array([[0], [1], [1], [0]])
-W1 = np.array([[0.10, 0.20],
-                 [0.30, 0.40]])
-W2 = np.array([[0.50], [0.60]])
-B1 = np.array([0.1, 0.2])
-B2 = np.array([0.3])   
+
+W1 = 2*np.random.random((inputs, hiddens))-1
+W2 = 2*np.random.random((hiddens, outputs))-1
+B1 = np.zeros(hiddens)
+B2 = np.zeros(outputs)
 
 # 순방향 전파 계산
 def predict(x):
-        layer0 = x			# 입력을 layer0에 대입한다. 
-        Z1 = np.dot(layer0, W1)+B1	# 행렬의 곱을 계산한다. 
-        layer1 = actf(Z1)		# 활성화 함수를 적용한다. 
-        Z2 = np.dot(layer1, W2)+B2	# 행렬의 곱을 계산한다. 
-        layer2 = actf(Z2)		# 활성화 함수를 적용한다. 
-        return layer0, layer1, layer2
+    layer0 = x
+    Z1 = np.dot(layer0, W1)+B1
+    layer1 = actf(Z1)
+    Z2 = np.dot(layer1, W2)+B2
+    layer2 = actf(Z2)
+    return layer0, layer1, layer2
+
+# 역방향 전파 계산
+def fit():
+    global W1, W2, B1, B2
+    for i in range(60000):
+        layer0, layer1, layer2 = predict(X)
+        layer2_error = layer2-T
+        
+        layer2_delta = layer2_error*actf_deriv(layer2)
+        layer1_error = np.dot(layer2_delta, W2.T)
+        layer1_delta = layer1_error*actf_deriv(layer1)
+        
+        W2 += -learning_rate*np.dot(layer1.T, layer2_delta)/4.0
+        W1 += -learning_rate*np.dot(layer0.T, layer1_delta)/4.0
+        B2 += -learning_rate*np.sum(layer2_delta, axis = 0)/4.0
+        B1 += -learning_rate*np.sum(layer1_delta, axis = 0)/4.0
+        
 def test():
     for x, y in zip(X, T):
-        x = np.reshape(x, (1, -1))	# x를 2차원 행렬로 만든다.입력은 2차원이어야 한다.
+        x = np.reshape(x, (1, -1))
         layer0, layer1, layer2 = predict(x)
         print(x, y, layer2)
-test() 
+        
+print("\np.250 미니배치 구현")        
+fit()
+test()
+''' 
+###
+
+# MNIST 숫자 인식 케라스를 이용해서 정확률 확인
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+
+model = tf.keras.models.Sequential()
+
+model.add(tf.keras.layers.Dense(512, activation='relu', input_shape=(784,)))
+model.add(tf.keras.layers.Dense(512, activation='relu'))
+model.add(tf.keras.layers.Dense(10, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop', loss='mse', metrics=['accuracy'])
+
+train_images = train_images.reshape((60000, 784))
+train_images = train_images.astype('float32') / 255.0
+
+test_images = test_images.reshape((10000, 784))
+test_images = test_images.astype('float32') / 255.0
+
+train_labels = tf.keras.utils.to_categorical(train_labels)
+test_labels = tf.keras.utils.to_categorical(test_labels)
+
+model.fit(train_images, train_labels, epochs=5, batch_size=50)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('테스트 정확도:', test_acc)
+
+import cv2 as cv
+
+image = cv.imread('test.png', cv.IMREAD_GRAYSCALE)
+image = image.astype('float32')
+image = image.reshape(1, 784)
+image = 255-image
+image /= 255.0
+
+plt.imshow(image.reshape(28, 28),cmap='Greys')
+plt.show()
+
+pred = model.predict(image.reshape(1, 784), batch_size=1)
+print("추정된 숫자=", pred.argmax())
+
+###
+
+# 그림판에 임의 숫자 그린 후 인식되는지 확인
+# a) 은닉층 유닛 개수 조절 후 정확률 확인 (2배 or 1/2배로)
+# x2 0.97 -> 0.98 x0.5 0.979 -> 0.978
+# b) 배치 크기 조절 후 정확률 확인
+# 0.979 -> 0.98
+# c) relu -> 시그모이드로 바꾸고 정확률 확인
+# 연습문제 9번(레이어 추가) 10, 11, 12, 13, 14
